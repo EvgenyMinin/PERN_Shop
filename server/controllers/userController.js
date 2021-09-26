08d1d7bda0bec947f1bf4dc7bd4dc28ef5c1fa31
EvgenyMinin <evgeny.frontend@gmail.com>
2021-09-26 21:27:42 +0300

    added error handling middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,18 @@
+const ApiError = require("../error/ApiError");
+
 class UserController {
-    async registration(req, res) {
+    async registration(req, res) {}
 
-    }
+    async login(req, res) {}
 
-    async login(req, res) {
-
-    }
-
-    async check(req, res) {
-
+    async check(req, res, next) {
+        const {
+            query: { id },
+        } = req;
+        if (!id) {
+            return next(ApiError.badRequest('Не задан ID'));
+        }
+        res.json(id);
     }
 }
 
