f8950523ab270d273c69ff301b6ab2d4de3af385
EvgenyMinin <evgeny.frontend@gmail.com>
2021-10-23 16:47:13 +0300

    added fetching device by id

--- client/src/pages/Device.jsx
+++ client/src/pages/Device.jsx
@@ -1,52 +1,45 @@
-import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 
 import deviceStar from "../images/deviceStar.png";
+import { fetchDeviceById } from "../http/deviceAPI";
 
-const Device = observer(() => {
-    const { img, name, rating, price } = {
-        id: 3,
-        name: "IPhone 12 pro",
-        price: 80000,
-        rating: 5,
-        img: "https://static.re-store.ru/upload/resize_cache/iblock/c17/1160_880_17f5c944b3b71591cc9304fac25365de2/c17ffd485dd8adae3cbc7f2f992fa624.jpg",
-    };
+const Device = () => {
+    const [device, setDevice] = useState({ info: [] });
+    const { id } = useParams();
 
-    const description = [
-        { id: 1, title: "Оперативная память", description: "6 гб" },
-        { id: 2, title: "Камера", description: "12 мп" },
-        { id: 3, title: "Процессор", description: "Ryzen" },
-        { id: 4, title: "Кол-во ядер", description: "8" },
-        { id: 5, title: "Аккумулятор", description: "4000" },
-    ];
+    useEffect(() => {
+        fetchDeviceById(id).then((data) => setDevice(data));
+    }, []);
+
     return (
         <Container className="mt-3">
             <Row>
                 <Col md={4}>
-                    <Image height={300} src={img} />
+                    <Image height={300} src={`${process.env.REACT_APP_API_URL}/${device.img}`} />
                 </Col>
                 <Col md={4}>
                     <Row className="d-flex flex-column align-items-center">
-                        <h2 style={{ textAlign: "center" }}>{name}</h2>
+                        <h2 style={{ textAlign: "center" }}>{device.name}</h2>
                         <div
                             className="d-flex align-items-center justify-content-center p-5"
                             style={{ background: `url(${deviceStar}) no-repeat center center`, backgroundSize: "contain", fontSize: 64 }}
                         >
-                            {rating}
+                            {device.rating}
                         </div>
                     </Row>
                 </Col>
                 <Col md={4}>
                     <Card className="d-flex flex-column align-items-center justify-content-around" style={{ height: 300 }}>
-                        <h3>От: {price.toLocaleString("ru-RU")} руб.</h3>
+                        <h3>От: {device.price?.toLocaleString("ru-RU")} руб.</h3>
                         <Button variant="success">Добавить в корзину</Button>
                     </Card>
                 </Col>
             </Row>
             <Row className="mt-3">
                 <h2>Характеристики: </h2>
-                {description.map(({ id, title, description }, index) => (
+                {device.info?.map(({ id, title, description }, index) => (
                     <Row key={id} style={{ backgroundColor: index % 2 === 0 ? "lightgray" : "transparent" }} className="py-2">
                         {title}: {description}
                     </Row>
@@ -54,6 +47,6 @@ const Device = observer(() => {
             </Row>
         </Container>
     );
-});
+};
 
 export default Device;
